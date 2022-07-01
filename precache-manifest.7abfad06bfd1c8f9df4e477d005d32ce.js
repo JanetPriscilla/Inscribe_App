e26f61b4ed6117b526e8fed60775048a35d567d5
JanetPriscilla <87714593+JanetPriscilla@users.noreply.github.com>
2022-07-01 10:51:57 +0200

    Updates

---
+++ precache-manifest.7abfad06bfd1c8f9df4e477d005d32ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a973d8884e53863483",
+    "url": "/Inscribe_App/static/js/main.77a973d8.chunk.js"
+  },
+  {
+    "revision": "57b7cc6075c31bd7f98b",
+    "url": "/Inscribe_App/static/js/1.57b7cc60.chunk.js"
+  },
+  {
+    "revision": "5b358236daf018bb1856",
+    "url": "/Inscribe_App/static/js/runtime~main.5b358236.js"
+  },
+  {
+    "revision": "f721ca7cb13c146e9579c9db5e39d639",
+    "url": "/Inscribe_App/index.html"
+  }
+];
